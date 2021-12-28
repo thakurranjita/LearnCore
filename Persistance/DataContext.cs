91dc89ce124bf7d78121c5d303854cdcd5a28b6d
Ranjita Thakur <Ranjita.Thakur@cdk.com>
2021-12-28 17:28:06 +0530

    End of Section 2

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,13 @@
+using DOmain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
